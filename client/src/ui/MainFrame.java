9ec92bad357c87713a7c6fce284462b748212914
wza <wzian@126.com>
2015-10-22 10:52:02 +0800

    新建ui包

---
+++ client/src/ui/MainFrame.java
@@ -0,0 +1,24 @@
+package ui;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public static void main(String[] args) {
+		new MainFrame();
+	}
+	
+	public MainFrame(){
+		initMainFrame();
+	}
+
+	private void initMainFrame() {
+	}
+
+
+}
