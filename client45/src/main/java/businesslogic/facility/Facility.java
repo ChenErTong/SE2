59a99c35d2669bf5ae361ae200ada8505492a6ab
wza <wzian@126.com>
2015-11-21 17:08:46 +0800

    去除级调用

--- client45/src/main/java/businesslogic/facility/Facility.java
+++ client45/src/main/java/businesslogic/facility/Facility.java
@@ -6,16 +6,18 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 import dataservice.facilitydataservice.FacilityDataService;
+import po.FacilityPO;
 import state.ConfirmState;
 import state.ResultMessage;
 import vo.FacilityVO;
 
-public class Facility{
+public class Facility {
 	private FacilityDataService facilityData;
-	
+
 	public Facility() {
 		try {
-			facilityData = (FacilityDataService)Naming.lookup("rmi://" + "127.0.0.1" + ":" + "8888" + "/"+FacilityDataService.NAME);
+			facilityData = (FacilityDataService) Naming
+					.lookup("rmi://" + "127.0.0.1" + ":" + "8888" + "/" + FacilityDataService.NAME);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (RemoteException e) {
@@ -24,23 +26,29 @@ public class Facility{
 			e.printStackTrace();
 		}
 	}
+
 	public ConfirmState confirmOperation() {
 		return ConfirmState.CONFIRM;
 	}
 
 	public ResultMessage addFacility(FacilityVO facility) throws RemoteException {
-			return facilityData.add(FacilityTrans.convertVOtoPO(facility));
+		FacilityPO facilityPO = FacilityTrans.convertVOtoPO(facility);
+		return facilityData.add(facilityPO);
 	}
 
 	public FacilityVO findFacility(String facilityId) throws RemoteException {
-		return FacilityTrans.convertPOtoVO(facilityData.find(facilityId));
-	}
-	public ResultMessage modifyFacility(FacilityVO facility)  throws RemoteException{
-			return facilityData.modify(FacilityTrans.convertVOtoPO(facility));
-	}
-	public ResultMessage deleteFacility(FacilityVO facility)  throws RemoteException{
-			return facilityData.delete(facility.getFacilityIdString());
+		FacilityPO facilityPO = facilityData.find(facilityId);
+		FacilityVO facilityVO = FacilityTrans.convertPOtoVO(facilityPO);
+		return facilityVO;
 	}
 
+	public ResultMessage modifyFacility(FacilityVO facility) throws RemoteException {
+		FacilityPO facilityPO = FacilityTrans.convertVOtoPO(facility);
+		return facilityData.modify(facilityPO);
+	}
+
+	public ResultMessage deleteFacility(FacilityVO facility) throws RemoteException {
+		return facilityData.delete(facility.getFacilityIdString());
+	}
 
 }
--- client45/src/main/java/businesslogic/facility/FacilityTrans.java
+++ client45/src/main/java/businesslogic/facility/FacilityTrans.java
@@ -1,13 +1,33 @@
 package businesslogic.facility;
 
+import java.util.ArrayList;
+
 import po.FacilityPO;
+import po.receiptpo.LoadingListPO;
 import vo.FacilityVO;
 
 public class FacilityTrans {
-	public static FacilityPO convertVOtoPO(FacilityVO vo){
-		return new FacilityPO(vo.getFacilityIdString(), vo.getDateString(), vo.getManagerId(), vo.getDeliverHistory());
+	public static FacilityPO convertVOtoPO(FacilityVO vo) {
+		if (vo == null)
+			return null;
+		else {
+			String ID = vo.getFacilityIdString();
+			String date = vo.getDateString();
+			String manageID = vo.getManagerId();
+			ArrayList<LoadingListPO> deliverHistory = vo.getDeliverHistory();
+			return new FacilityPO(ID, date, manageID, deliverHistory);
+		}
 	}
-	public static FacilityVO convertPOtoVO(FacilityPO po){
-		return new FacilityVO(po.getManagerId(), po.getDeliverHistory(), po.getID(), po.getDate());
+
+	public static FacilityVO convertPOtoVO(FacilityPO po) {
+		if (po == null)
+			return null;
+		else{
+			String ID = po.getID();
+			String date = po.getDate();
+			String manageID = po.getManagerId();
+			ArrayList<LoadingListPO> deliverHistory = po.getDeliverHistory();
+			return new FacilityVO(manageID, deliverHistory, ID, date);
+		}
 	}
 }
