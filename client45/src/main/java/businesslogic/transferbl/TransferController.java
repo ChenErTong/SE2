8f609570d99eb84b8e49ffbeb91cc4aaa0b1d5a7
LIUXUANLIN <1304317390@qq.com>
2015-11-23 20:47:19 +0800

    transferBL和transferController

---
+++ client45/src/main/java/businesslogic/transferbl/TransferController.java
@@ -0,0 +1,81 @@
+package businesslogic.transferbl;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import businesslogicservice.transferblservice.TransferBLService;
+import po.receiptpo.ReceiptPO;
+import state.CommodityState;
+import state.ConfirmState;
+import state.ResultMessage;
+import vo.CommodityVO;
+import vo.receiptvo.TransferArrivalListVO;
+import vo.receiptvo.TransferOrderVO;
+
+public class TransferController implements TransferBLService{
+    Transfer TransferBL;
+	@Override
+	public ConfirmState confirmOperation() {
+		
+		return TransferBL.confirmOperation();
+	}
+
+	@Override
+	public ArrayList<CommodityVO> getAllCommodities() {
+		
+		return TransferBL.getAllCommodities();
+	}
+
+	@Override
+	public TransferOrderVO planeTransfer(String facilityID, String departure, String destination, String courierName,
+			ArrayList<String> orders) {
+		
+		return TransferBL.planeTransfer(facilityID, departure, destination, courierName, orders);
+	}
+
+	@Override
+	public TransferOrderVO truckTransfer(String facilityID, String departure, String destination, String courierName,
+			ArrayList<String> orders) {
+		
+		return TransferBL.truckTransfer(facilityID, departure, destination, courierName, orders);
+	}
+
+	@Override
+	public TransferOrderVO trainTransfer(String facilityID, String departure, String destination, String courierName,
+			ArrayList<String> orders) {
+		
+		return TransferBL.trainTransfer(facilityID, departure, destination, courierName, orders);
+	}
+
+	@Override
+	public ResultMessage submit(ReceiptPO receipt) {
+		
+		try {
+			return TransferBL.submit(receipt);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public ResultMessage save(ReceiptPO receipt) {
+		
+		try {
+			return TransferBL.save(receipt);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public TransferArrivalListVO receiptList(String transferID, String departure, String destination,
+			CommodityState state, ArrayList<String> orders) {
+		
+		return TransferBL.receiptList(transferID, departure, destination, state, orders);
+	}
+
+}
