5b9e5b9c5798c537366b140290438837e7f53f82
wza <wzian@126.com>
2015-11-23 23:14:19 +0800

    BaseData还要改接口

--- server45/src/main/java/data/basedata/BaseData.java
+++ server45/src/main/java/data/basedata/BaseData.java
@@ -20,7 +20,22 @@ public class BaseData extends ManageData<BasePO> implements BaseDataService{
 		poList=new SerSaveAndLoad<BasePO>("data/"+NAME+".ser");
 	}
 	public ArrayList<BasePO> find(FindTypeBase baseType) throws RemoteException {
-		return null;
+		//改接口
+		ArrayList<BasePO> bankAccounts = new ArrayList<>();
+		for (int i = 0; i < poList.size(); i++) {
+			BasePO po = poList.get(i);
+			if (baseType == FindTypeBase.SalaryPolicy) {
+					// 如果SalaryPolicy包含关键字就返回
+					bankAccounts.add(po);
+			}else if (baseType==FindTypeBase.DISTANCE) {
+					// 如果DISTANCE包含关键字就返回
+					bankAccounts.add(po);
+			}else if (baseType==FindTypeBase.PRICE) {
+					// 如果PRICE包含关键字就返回
+					bankAccounts.add(po);
+			}
+		}
+		return bankAccounts;
 	}
 
 }
