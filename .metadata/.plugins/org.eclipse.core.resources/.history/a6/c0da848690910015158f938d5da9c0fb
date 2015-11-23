f79c01bc20263165a59012161bf4b320b263f7de
LIUXUANLIN <1304317390@qq.com>
2015-11-23 11:16:43 +0800

    ReceiptController

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/a6/c0da848690910015158f938d5da9c0fb
@@ -0,0 +1,5 @@
+package businesslogic.receiptbl;
+
+public class ReceiptController {
+
+}
---
+++ client45/src/main/java/businesslogic/receiptbl/ReceiptController.java
@@ -0,0 +1,44 @@
+package businesslogic.receiptbl;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import businesslogicservice.receiptblservice.ReceiptBLService;
+import po.receiptpo.ReceiptPO;
+import state.ReceiptState;
+import state.ReceiptType;
+import state.ResultMessage;
+import vo.receiptvo.ReceiptVO;
+
+public class ReceiptController implements ReceiptBLService{
+    Receipt ReceiptBL;
+	@Override
+	public ResultMessage updateReceipt(ReceiptVO receiptVO, ReceiptType receiptType) {
+		try {
+			return ReceiptBL.updateReceipt(receiptVO, receiptType);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public ArrayList<ReceiptPO> passReceipt(ArrayList<ReceiptVO> VOs, ReceiptType receiptTypes) {
+		
+		return ReceiptBL.passReceipt(VOs, receiptTypes);
+	}
+
+	@Override
+	public ArrayList<ReceiptPO> dontPassReceipt(ArrayList<ReceiptVO> VOs, ReceiptType receiptTypes) {
+		
+		return ReceiptBL.dontPassReceipt(VOs, receiptTypes);
+	}
+
+	@Override
+	public ArrayList<ReceiptVO> showReceipt(ReceiptState receiptStates) {
+		
+		return ReceiptBL.showReceipt(receiptStates);
+	}
+
+}
