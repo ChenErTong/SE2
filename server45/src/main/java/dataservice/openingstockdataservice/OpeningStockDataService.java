92934b7677674f123b45124e2f0b769ab64314ee
wza <wzian@126.com>
2015-11-23 13:11:16 +0800

    rmi debug

--- server45/src/main/java/dataservice/openingstockdataservice/OpeningStockDataService.java
+++ server45/src/main/java/dataservice/openingstockdataservice/OpeningStockDataService.java
@@ -10,7 +10,7 @@ public interface OpeningStockDataService extends DataService<OpeningStockPO> {
 	/** 接口的名称，RMI绑定时候的名称 */
 	public static final String NAME = "OpeningStockData";
 	public ResultMessage add(OpeningStockPO po) throws RemoteException;
-	public String getID();
+	public String getID() throws RemoteException;
 	public void init() throws RemoteException;
 	public OpeningStockPO findOpeningStock(String id) throws RemoteException;
 }
