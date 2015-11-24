a334f95abc170dc2b7caf7281a37b8646cbc9d7f
wza <wzian@126.com>
2015-11-24 11:51:58 +0800

    导出excel

--- client45/src/test/java/output/TestExcel.java
+++ client45/src/test/java/output/TestExcel.java
@@ -1,20 +1,21 @@
-//package output;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import util.ExportExcel;
-//import vo.ExpenseVO;
-//
-//public class TestExcel {
-//	public static void main(String[] args) {
-//		String[] title = {"ID","值"};
-//		List<Object> expenses = new ArrayList<Object>();
-//		for (int i = 0; i < 80; i++) {
-//			ExpenseVO vo = new ExpenseVO(i+"", "南京", i*100, "南京");
-//			expenses.add(vo);
-//		}
-//		String fileName = "testExcel";
-//		ExportExcel.exportExcel(fileName, title, expenses);
-//	}
-//}
+package output;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import util.ExportExcel;
+import vo.ExpenseVO;
+
+public class TestExcel {
+	public static void main(String[] args) {
+		String[] title = {"ID","地区","价格","又一个地区","时间"};
+		List<Object> expenses = new ArrayList<Object>();
+		for (int i = 0; i < 80; i++) {
+			ExpenseVO vo = new ExpenseVO(i+"", "南京", i*100, "南京");
+			expenses.add(vo);
+		}
+		String fileName = "output/testExcel.xls";
+		String result = ExportExcel.exportExcel(fileName, title, expenses);
+		System.out.println(result);
+	}
+}
