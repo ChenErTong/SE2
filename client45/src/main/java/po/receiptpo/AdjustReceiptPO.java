bcd4e6108c0295089f00141cc8b1e230866d2bce
wza <wzian@126.com>
2015-11-24 17:48:36 +0800

    Revert "。。"
    This reverts commit 9fa87dc858a8b72a951a3ed2e6b7cb0e8d4563b7.

--- client45/src/main/java/po/receiptpo/AdjustReceiptPO.java
+++ client45/src/main/java/po/receiptpo/AdjustReceiptPO.java
@@ -1,5 +1,7 @@
 package po.receiptpo;
 
+import state.ReceiptType;
+
 public class AdjustReceiptPO extends ReceiptPO{
 	
 	  /** @author lxl
@@ -8,9 +10,9 @@ public class AdjustReceiptPO extends ReceiptPO{
 	
 	 
 	private static final long serialVersionUID = 1L;
-	public AdjustReceiptPO(String ID, String exA, String exB, String exC, String exD, String aftA, String aftB,
-			String aftC, String aftD) {
-		super(ID);
+	public AdjustReceiptPO(String ID, ReceiptType type,int exA, int exB, int exC, int exD, int aftA, int aftB,
+			int aftC, int aftD) {
+		super(ID, type);
 		this.exA = exA;
 		this.exB = exB;
 		this.exC = exC;
@@ -20,13 +22,62 @@ public class AdjustReceiptPO extends ReceiptPO{
 		this.aftC = aftC;
 		this.aftD = aftD;
 	}
-	public String exA;
-	public String exB;
-	public String exC;
-	public String exD;
+	public int exA;
+	public int exB;
+	public int exC;
+	public int exD;
 	
-	public String aftA;
-	public String aftB;
-	public String aftC;
-	public String aftD;
+	public int aftA;
+	public int aftB;
+	public int aftC;
+	public int aftD;
+	public int getExA() {
+		return exA;
+	}
+	public void setExA(int exA) {
+		this.exA = exA;
+	}
+	public int getExB() {
+		return exB;
+	}
+	public void setExB(int exB) {
+		this.exB = exB;
+	}
+	public int getExC() {
+		return exC;
+	}
+	public void setExC(int exC) {
+		this.exC = exC;
+	}
+	public int getExD() {
+		return exD;
+	}
+	public void setExD(int exD) {
+		this.exD = exD;
+	}
+	public int getAftA() {
+		return aftA;
+	}
+	public void setAftA(int aftA) {
+		this.aftA = aftA;
+	}
+	public int getAftB() {
+		return aftB;
+	}
+	public void setAftB(int aftB) {
+		this.aftB = aftB;
+	}
+	public int getAftC() {
+		return aftC;
+	}
+	public void setAftC(int aftC) {
+		this.aftC = aftC;
+	}
+	public int getAftD() {
+		return aftD;
+	}
+	public void setAftD(int aftD) {
+		this.aftD = aftD;
+	}
+	
 }
--- server45/src/main/java/po/FacilityPO.java
+++ server45/src/main/java/po/FacilityPO.java
@@ -21,19 +21,8 @@ public class FacilityPO extends PersistentObject{
 	 * 运输历史
 	 */
 	private ArrayList<LoadingListPO> deliverHistory;
-	/**
-	 * 底盘号
-	 */
-	private String bottomCode;
-	/**
-	 * 发动机号
-	 */
-	private String engineCode;
-	/**
-	 * 车牌号
-	 */
-	private String vehicleIdentificationNumber;
-	public FacilityPO(String id,String date, String managerId, ArrayList<LoadingListPO> deliverHistory) {
+
+	public FacilityPO(String id, String date, String managerId, ArrayList<LoadingListPO> deliverHistory) {
 		super(id, date);
 		this.managerId = managerId;
 		this.deliverHistory = deliverHistory;
@@ -50,33 +39,6 @@ public class FacilityPO extends PersistentObject{
 	public String getDate() {
 		return date;
 	}
-
-	public String getBottomCode() {
-		return bottomCode;
-	}
-
-	public String getEngineCode() {
-		return engineCode;
-	}
-
-	public String getVehicleIdentificationNumber() {
-		return vehicleIdentificationNumber;
-	}
-
-	public FacilityPO(String id,String managerId, String date, ArrayList<LoadingListPO> deliverHistory,
-			String bottomCode, String engineCode, String vehicleIdentificationNumber) {
-		super(id);
-		this.managerId = managerId;
-		this.date = date;
-		this.deliverHistory = deliverHistory;
-		this.bottomCode = bottomCode;
-		this.engineCode = engineCode;
-		this.vehicleIdentificationNumber = vehicleIdentificationNumber;
-	}
-
-	
-	
-	
 
 
 }
