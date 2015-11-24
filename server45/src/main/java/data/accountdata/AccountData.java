48ac583745b03d223360ef6c840e75df9d7e855f
wza <wzian@126.com>
2015-11-24 23:35:39 +0800

    空针debug

--- server45/src/main/java/data/accountdata/AccountData.java
+++ server45/src/main/java/data/accountdata/AccountData.java
@@ -5,6 +5,7 @@ import java.rmi.RemoteException;
 import data.ManageData;
 import dataservice.accountdataservice.AccountDataService;
 import po.accountpo.AccountPO;
+import util.SerSaveAndLoad;
 
 public class AccountData extends ManageData<AccountPO> implements AccountDataService{
 
@@ -14,7 +15,7 @@ public class AccountData extends ManageData<AccountPO> implements AccountDataSer
 	private static final long serialVersionUID = 1L;
 	//poList在Data里,虽然每个Data的继承类都以Data为父类，但他们拥有不同的poList
 	public AccountData() throws RemoteException {
-		// TODO Auto-generated constructor stub
+		poList = new SerSaveAndLoad<AccountPO>("data/"+NAME+".ser");
 	}
 
 	public void init() throws RemoteException {
