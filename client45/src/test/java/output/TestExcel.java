311cefc0f585929e8240e921e01c0c262df18744
wza <wzian@126.com>
2015-11-24 11:55:17 +0800

    红叉，再同步一下

--- client45/src/test/java/output/TestExcel.java
+++ client45/src/test/java/output/TestExcel.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import util.ExportExcel;
-import vo.ExpenseVO;
+import vo.receiptvo.ExpenseVO;
 
 public class TestExcel {
 	public static void main(String[] args) {
