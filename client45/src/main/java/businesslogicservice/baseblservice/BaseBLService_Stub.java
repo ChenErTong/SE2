604f505bef1835b76acf77cbdaf9981d81be8e5c
wza <wzian@126.com>
2015-11-25 17:32:40 +0800

    VO改完了

--- client45/src/main/java/businesslogicservice/baseblservice/BaseBLService_Stub.java
+++ client45/src/main/java/businesslogicservice/baseblservice/BaseBLService_Stub.java
@@ -7,20 +7,20 @@ import state.FindTypeBase;
 import state.ResultMessage;
 import vo.BaseVO;
 
-public class BaseBLService_Stub implements BaseBLService{
+public class BaseBLService_Stub implements BaseBLService {
 
 	@Override
 	public ConfirmState confirmOperation() {
 		System.out.println("Please confirm your operation!");
-		
+
 		return ConfirmState.CANCEL;
 	}
 
 	@Override
 	public ArrayList<BaseVO> show(FindTypeBase baseType) {
-		System.out.println("Show"+ baseType +"base value!");
-		
-		return new ArrayList<BaseVO>(){};
+		System.out.println("Show" + baseType + "base value!");
+
+		return new ArrayList<BaseVO>();
 	}
 
 	@Override
@@ -38,14 +38,14 @@ public class BaseBLService_Stub implements BaseBLService{
 	@Override
 	public ResultMessage deleteBase(String ID) {
 		System.out.println("Delete a base!");
-		return  ResultMessage.SUCCESS;
+		return ResultMessage.SUCCESS;
 	}
 
 	@Override
 	public ResultMessage updateBase(BaseVO vo) {
 		System.out.println("The value of the base has been modified!");
 		return ResultMessage.SUCCESS;
-		
+
 	}
 
 }
