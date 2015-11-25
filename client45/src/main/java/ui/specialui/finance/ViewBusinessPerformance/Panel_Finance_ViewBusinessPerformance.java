23ed186c3766790fcf37b7394c51bfd639c2fad8
ZepPellN <283633780@qq.com>
2015-11-25 21:23:31 +0800

    查看成本收益表和经营情况表。

--- client45/src/main/java/ui/specialui/finance/ViewBusinessPerformance/Panel_Finance_ViewBusinessPerformance.java
+++ client45/src/main/java/ui/specialui/finance/ViewBusinessPerformance/Panel_Finance_ViewBusinessPerformance.java
@@ -4,12 +4,11 @@ import ui.myui.MyJButton;
 import ui.myui.MyJLabel;
 import ui.myui.MyJPanel;
 import ui.specialui.finance.Frame_Finance;
+import ui.specialui.manager.ViewBusinessPerformance.Panel_Manager_BusinessPerformance;
 
 public class Panel_Finance_ViewBusinessPerformance extends MyJPanel{
 	private Panel_Finance_BusinessPerformance businessPerformance;
-	private Panel_Finance_SearchPerformance searchPerformance;
-	private MyJButton ViewBusinessState;
-	private MyJButton ExportBusinessState;
+	private MyJButton ExportBusinessTable;
 	private static final long serialVersionUID = 1L;
 
 	public Panel_Finance_ViewBusinessPerformance(Frame_Finance frame_Finance) {
@@ -19,21 +18,14 @@ public class Panel_Finance_ViewBusinessPerformance extends MyJPanel{
 	}
 	private void initComponent(Frame_Finance frame_Finance) {
 		this.add(new MyJLabel(540,40,200,40,"查看经营情况表",24,true));
-		businessPerformance = new Panel_Finance_BusinessPerformance();
+		businessPerformance = new Panel_Finance_BusinessPerformance(frame_Finance);
 		this.add(businessPerformance);
 		
-		searchPerformance = new Panel_Finance_SearchPerformance(frame_Finance);
-		this.add(searchPerformance);
-		
-		ViewBusinessState = new MyJButton(460, 673, 150, 40,"经营情况表预览",14);
-		ViewBusinessState.setActionCommand("ViewBusinessState");
-		ViewBusinessState.addActionListener(frame_Finance);
-		this.add(ViewBusinessState);
-		
-		ExportBusinessState = new MyJButton(1040,673,150,40,"导出经营情况表",14);
-		ExportBusinessState.setActionCommand("ExportBusinessState");
-		ExportBusinessState.addActionListener(frame_Finance);
-		this.add(ExportBusinessState);
+		ExportBusinessTable = new MyJButton(1040,673,150,40,"导出经营情况表",14);
+		ExportBusinessTable.setActionCommand("ExportBusinessTable");
+		ExportBusinessTable.addActionListener(frame_Finance);
+		this.add(ExportBusinessTable);
+		ExportBusinessTable.setVisible(true);
 		
 	}
 }
--- client45/src/main/java/ui/specialui/finance/ViewIncomeState/Panel_Finance_ViewIncomeStatement.java
+++ client45/src/main/java/ui/specialui/finance/ViewIncomeState/Panel_Finance_ViewIncomeStatement.java
@@ -4,43 +4,30 @@ import ui.myui.MyJButton;
 import ui.myui.MyJLabel;
 import ui.myui.MyJPanel;
 import ui.specialui.finance.Frame_Finance;
+import ui.specialui.manager.ViewIncomeState.Panel_Manager_IncomeState;
 
 
 public class Panel_Finance_ViewIncomeStatement extends MyJPanel{
 
 	private static final long serialVersionUID = 1L;
-	private Panel_Finance_SearchIncomeState searchPanel;
 	private Panel_Finance_IncomeState statePanel;
-	private MyJButton SelectedAllReceipts;
-	private MyJButton ViewIncomeTable;
-	private MyJButton ExportIncomeTable;
 	public Panel_Finance_ViewIncomeStatement(Frame_Finance frame_Finance) {
 		super(0, 0, 1280, 720);
 		this.setOpaque(false);
 		this.initComponent(frame_Finance);
 	}
 	private void initComponent(Frame_Finance frame_Finance) {
-		this.add(new MyJLabel(540,40,200,40,"查看成本收益表",24,true));
-		searchPanel = new Panel_Finance_SearchIncomeState(frame_Finance);
-		this.add(searchPanel);
+		this.add(new MyJLabel(550, 20, 210, 90, "查看成本收益表", 24, true));
 		
-		statePanel = new Panel_Finance_IncomeState();
+		statePanel = new Panel_Finance_IncomeState(frame_Finance);
 		this.add(statePanel);
 		
-		SelectedAllReceipts = new MyJButton(120, 673, 150, 40,"选中所有单据",14);
-		SelectedAllReceipts.setActionCommand("SelectedAllReceipts");
-		SelectedAllReceipts.addActionListener(frame_Finance);
-		this.add(SelectedAllReceipts);
 		
-		ViewIncomeTable = new MyJButton(380, 673, 150, 40,"成本收益表预览",14);
-		ViewIncomeTable.setActionCommand("ViewIncomeTable");
-		ViewIncomeTable.addActionListener(frame_Finance);
-		this.add(ViewIncomeTable);
-		
-		ExportIncomeTable = new MyJButton(1040,673,150,40,"导出成本收益表",14);
+		MyJButton ExportIncomeTable = new MyJButton(1040,673,150,40,"导出成本收益表",14);
 		ExportIncomeTable.setActionCommand("ExportIncomeTable");
 		ExportIncomeTable.addActionListener(frame_Finance);
 		this.add(ExportIncomeTable);
-	}
+		ExportIncomeTable.setVisible(true);
 
+}
 }
