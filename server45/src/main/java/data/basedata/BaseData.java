bac3b239df40814c6e58c4c0e5ed85c960c61086
wza <wzian@126.com>
2015-11-26 11:08:25 +0800

    config

--- server45/src/main/java/data/basedata/BaseData.java
+++ server45/src/main/java/data/basedata/BaseData.java
@@ -3,6 +3,7 @@ package data.basedata;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
+import config.XMLReader;
 import data.ManageData;
 import dataservice.basedataservice.BaseDataService;
 import po.BasePO;
@@ -18,9 +19,9 @@ public class BaseData extends ManageData<BasePO> implements BaseDataService{
 	//poList在Data里,虽然每个Data的继承类都以Data为父类，但他们拥有不同的poList
 	public BaseData() throws RemoteException {
 		poList=new SerSaveAndLoad<BasePO>("data/"+NAME+".ser");
-//		configReader=new XMLReader("config/"+NAME+".xml");
-//		maxID=Integer.parseInt(configReader.getValue("maxID"));
-//		IDMaxBit=Integer.parseInt(configReader.getValue("IDMaxBit"));
+		configReader=new XMLReader("config/"+NAME+".xml");
+		maxID=Integer.parseInt(configReader.getValue("maxID"));
+		IDMaxBit=Integer.parseInt(configReader.getValue("IDMaxBit"));
 	}
 	public ArrayList<BasePO> find(FindTypeBase baseType) throws RemoteException {
 		//改接口
