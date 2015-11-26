80d137b56cbdd1e641c6ac46e8ea5cc58a239155
wza <wzian@126.com>
2015-11-26 09:52:00 +0800

    Data填坑 getID方法普遍存在bug 我来改

--- server45/src/main/java/data/recorddata/RecordData.java
+++ server45/src/main/java/data/recorddata/RecordData.java
@@ -3,36 +3,57 @@ package data.recorddata;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
-import data.Data;
+import data.ManageData;
 import dataservice.recorddataservice.RecordDataService;
 import po.BussinessConditionPO;
 import po.BussinessProcessPO;
+import po.PersistentObject;
+import util.SerSaveAndLoad;
 
-public class RecordData extends Data implements RecordDataService {
+public class RecordData extends ManageData<PersistentObject> implements RecordDataService {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	public RecordData() throws RemoteException {
+		poList=new SerSaveAndLoad<PersistentObject>("data/"+NAME+".ser");
 	}
 
 	@Override
 	public ArrayList<BussinessProcessPO> getBussinessProcess(String begindate, String enddate) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList< BussinessProcessPO> pos = new ArrayList<>();
+		for (int i = 0; i < poList.size(); i++) {
+			PersistentObject po = poList.get(i);
+			if (inDate(po, begindate, enddate)) {
+				pos.add((BussinessProcessPO)po);
+			}
+		}
+		return pos;
 	}
 
 	@Override
 	public ArrayList<BussinessConditionPO> getBussinessCondition(String enddate) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList< BussinessConditionPO> pos = new ArrayList<>();
+		for (int i = 0; i < poList.size(); i++) {
+			PersistentObject po = poList.get(i);
+			if (po.getDate().compareTo(enddate) <= 0) {
+				pos.add((BussinessConditionPO)po);
+			}
+		}
+		return pos;
 	}
 
 	@Override
 	public String getBegin() throws RemoteException {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private boolean inDate(PersistentObject po, String beginDate, String endDate) {
+		if (po.getDate().compareTo(beginDate) >= 0 && po.getDate().compareTo(endDate) <= 0)
+			return true;
+		return false;
 	}
 
 }
