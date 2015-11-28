ad745ac4fd3280fa4989ee6718e323db1f3dfe9d
LIUXUANLIN <1304317390@qq.com>
2015-11-28 17:19:50 +0800

    添加save submit

--- client45/src/main/java/vo/receiptvo/InventoryExportReceiptVO.java
+++ client45/src/main/java/vo/receiptvo/InventoryExportReceiptVO.java
@@ -5,11 +5,27 @@ import state.ReceiptType;
 public class InventoryExportReceiptVO extends ReceiptVO {
 	/**目的地**/
 	public String destination;
+	public String depture;
 	/**装运形式**/
 	public String Transfer;
-	public InventoryExportReceiptVO(String ID,ReceiptType type,String destination, String transfer) {
-		super(ID,type);
+	public String TransferID;
+	public String CommoditiesID;
+	public int a;
+	public int b;
+	public int c;
+	public int d;
+	public InventoryExportReceiptVO(String id, ReceiptType type, String destination, String depture, String transfer,
+			String transferID, String commoditiesID, int a, int b, int c, int d) {
+		super(id, type);
 		this.destination = destination;
+		this.depture = depture;
 		Transfer = transfer;
+		TransferID = transferID;
+		CommoditiesID = commoditiesID;
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
 	}
+	
 }
--- client45/src/main/java/vo/receiptvo/InventoryImportReceiptVO.java
+++ client45/src/main/java/vo/receiptvo/InventoryImportReceiptVO.java
@@ -3,24 +3,32 @@ package vo.receiptvo;
 import state.ReceiptType;
 
 public class InventoryImportReceiptVO extends ReceiptVO{
-	 //目的地
-	public String destination;
-	//区号
-	 public int a;
-	//排号
-	 public int b;
-	//架号
-	 public int c;
-	//位号
-	 public int d;	
 	
-	public InventoryImportReceiptVO(String id,ReceiptType type, String destination, int a, int b, int c, int d) {
-		super(id, type );
-		this.destination = destination;
-		this.a = a;
-		this.b = b;
-		this.c = c;
-		this.d = d;
-	}
+	// 目的地
+		public String depture;
+		public String destination;
+		public String CommoditiesID;
+		// 区号
+		public int a;
+		// 排号
+		public int b;
+		// 架号
+		public int c;
+		// 位号
+		public int d;
+		public InventoryImportReceiptVO(String id, ReceiptType type, String depture, String destination,
+				String commoditiesID, int a, int b, int c, int d) {
+			super(id, type);
+			this.depture = depture;
+			this.destination = destination;
+			CommoditiesID = commoditiesID;
+			this.a = a;
+			this.b = b;
+			this.c = c;
+			this.d = d;
+		}
+
+	
+	
 
 }
