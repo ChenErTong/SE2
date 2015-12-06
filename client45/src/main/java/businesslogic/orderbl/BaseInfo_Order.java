413fb8151beab66074fb408cb4e9f34aea97e7e5
wza <wzian@126.com>
2015-12-06 15:58:50 +0800

    第二波注释

--- client45/src/main/java/businesslogic/orderbl/BaseInfo_Order.java
+++ client45/src/main/java/businesslogic/orderbl/BaseInfo_Order.java
@@ -1,6 +1,9 @@
 package businesslogic.orderbl;
 
 import java.rmi.RemoteException;
+
+import state.ExpressType;
+
 /**
  * 
  * @author Ann
@@ -9,14 +12,39 @@ import java.rmi.RemoteException;
 public interface BaseInfo_Order {
 	/**
 	 * 计算获得单次订单的预计到达日期
+	 * 
 	 * @author Ann
-	 * @param cityFrom 起始城市
-	 * @param cityTo 到达城市
-	 * @param begindate 开始运送日期
-	 * @param transSpeed 运送速度常数
-	 * @return 预计到达日期
+	 * @param cityFrom
+	 *            起始城市
+	 * @param cityTo
+	 *            到达城市
+	 * @param begindate
+	 *            开始运送日期
+	 * @param transSpeed
+	 *            运送速度常数
+	 * @return String型，预计到达日期，日期格式为yyyy-MM-dd
 	 * @throws RemoteException
+	 *             远程异常
 	 */
-	public String getArrialDateByCities(String cityFrom,String cityTo,String begindate,double transSpeed) throws RemoteException;
-	public double getArrialPriceByCities(String cityFrom,String cityTo,double weight,double priceConstant) throws RemoteException;
+	public String getArrialDateByCities(String cityFrom, String cityTo, String begindate, double transSpeed)
+			throws RemoteException;
+
+	/**
+	 * 计算获得单次订单的金额
+	 * 
+	 * @see ExpressType#priceConstant 成本比例常数
+	 * @param cityFrom
+	 *            起始城市
+	 * @param cityTo
+	 *            到达城市
+	 * @param weight
+	 *            订单总重
+	 * @param priceConstant
+	 *            成本比例常数
+	 * @return double型，计算获得单次订单的金额
+	 * @throws RemoteException
+	 *             远程异常
+	 */
+	public double getArrialPriceByCities(String cityFrom, String cityTo, double weight, double priceConstant)
+			throws RemoteException;
 }
