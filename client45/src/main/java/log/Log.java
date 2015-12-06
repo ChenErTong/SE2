d44aa21b4b81a69e0242cde6848569ea444babe5
wza <wzian@126.com>
2015-12-06 20:11:34 +0800

    log

--- client45/src/main/java/log/Log.java
+++ client45/src/main/java/log/Log.java
@@ -4,8 +4,8 @@ package log;
 * @author Ann
 * @version 创建时间：2015年12月5日 下午3:02:26 
 */
-public class LogBL {
-	public LogBL() {
+public class Log {
+	public Log() {
 		// TODO Auto-generated constructor stub
 	}
 }
