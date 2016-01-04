26da23dcfa5f8c99124d3f97bbe0be436e3195ad
Anko <shino.ann@outlook.com>
2016-01-04 10:25:18 +0800

    注释

--- client45/src/main/java/businesslogic/CommonBusinessLogic.java
+++ client45/src/main/java/businesslogic/CommonBusinessLogic.java
@@ -6,7 +6,25 @@ import po.PersistentObject;
 import state.ResultMessage;
 
 public interface CommonBusinessLogic<PO extends PersistentObject> {
+	/**
+	 * 添加命令
+	 * @param po 持久化数据
+	 * @return 添加结果
+	 * @throws RemoteException 网络异常
+	 */
 	public ResultMessage add(PO po) throws RemoteException;
+	/**
+	 * 删除命令
+	 * @param ID 要删除的持久化数据ID
+	 * @return 被删除的持久化数据
+	 * @throws RemoteException 网络异常
+	 */
 	public PO delete(String ID) throws RemoteException;
+	/**
+	 * 修改命令
+	 * @param po 要修改的持久化数据
+	 * @return 修改前的持久化数据
+	 * @throws RemoteException 网络异常
+	 */
 	public PO modify(PO po) throws RemoteException;
 }
